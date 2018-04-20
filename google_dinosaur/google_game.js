5c6b4be116d98a8c4accd3b30aa38154b4d2fb9e
lulalabana <k11543789@gmail.com>
2018-04-20 14:23:27 +0800

    Add files via upload

--- google_dinosaur/google_game.js
+++ google_dinosaur/google_game.js
@@ -6,14 +6,17 @@ var treex=1600;
 var speed=0;
 var time=0;
 var dinosaury=300;
-var jump=30;
+var jump=-30;
 var jumpcheak=0;
 var treecheak=0;
+var gameover=0;
+var overy=305;
 
 function preload() {
   cloud = loadImage('cloud.png');
   dinosaur1 = loadImage('dinosaur1.png');
   dinosaur2 = loadImage('dinosaur2.png');
+  dinosaur3 = loadImage('dinosaur3.png');
   tree = loadImage('tree.png');
   ground = loadImage('ground.png');
   speed=10;
@@ -33,7 +36,6 @@ function setup() {
 function draw() {
   var sec = second();
   background(255,255,255);
-  time=time+1;
   //cloud
   for(var i=0;i<4;i++){
     image(cloud,cloudx[i],cloudy[i],80,50);
@@ -62,26 +64,28 @@ function draw() {
     image(dinosaur2, 250,dinosaury,132,120);
   }
   //jump
-
-  
   if(jumpcheak==1){
-    
+    if(gameover==1&&dinosaury>180){
+      overy=dinosaury;
+    }      
+    if(gameover==1){
+      overy=dinosaury;
+    }
     dinosaury=dinosaury-jump;
-    if(jump>-20){
-      jump=jump-2;
+    if(jump>-30){
+      jump=jump-1;
     }
     
     if(dinosaury>=300){
       jumpcheak=0;
-      jump=30;
+      jump=-30;
       dinosaury=300;
     }
     
   }
   
   //tree
-  
-  if(sec%6==0){
+  if(sec%5==0){
     treecheak=1;
   }
   if(treecheak==1){
@@ -93,9 +97,33 @@ function draw() {
     treex=1650;
   }
   
+  //game over
+  
+  if(treex==310&&jump<0){
+    gameover=1;
+  }
   
   
   //goal
+  if(gameover==0){
+   time=time+1; 
+  }
+  //over screen
+  if(gameover==1&&dinosaury>180){
+    background(255,255,255);
+    textSize(180);
+    text('GAMEOVER', 100, 300);
+    image(tree, treex+speed, 220,150,180);
+    image(ground, groundx1+speed, 300,1600,200);
+    image(ground, groundx2+speed, 300,1600,200);
+    speed=0;
+    for(var i=0;i<4;i++){
+      image(cloud,cloudx[i],cloudy[i],80,50);
+    }
+    image(dinosaur3, 267,overy,110,100);
+    noLoop();
+  }
+  
   fill(200, 200, 200);
   noStroke();
   rect(1320,36,150, 50);
@@ -103,12 +131,12 @@ function draw() {
   fill(0,0,0);
   textStyle(BOLD);
   text(time/10,1350,75);
-  text( sec ,1350,150);
 }
 
   function mouseClicked() {
-    image(tree, 900, 220,150,180);
-    if(jumpcheak==0){
+    if(jumpcheak==0&&gameover==0){
       jumpcheak=1;
+      jump=20;
     }
+    
   }
